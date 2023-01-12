b265bd85586b8dd4d407d788161c9b27f1b036b5
flyinacres <ron-github@rfgf.com>
2023-01-11 19:20:08 -0800

    Adding Note sub-component

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -22,25 +22,17 @@
             </div>
         </div>
 
-        <div 
+        <Note 
             v-for="note in notes" 
             :key ="notes.id"
-            class="card mb-4">
-            <div class="card-content">
-                <div class="content">
-                    {{ note.content }}
-                </div>
-            </div>
-            <footer class="card-footer">
-                <a href="#" class="card-footer-item">Edit</a>
-                <a href="#" class="card-footer-item">Delete</a>
-            </footer>
-        </div>
+            :note="note"
+        />
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import Note from '@/components/Notes/Note.vue'
 
 const newNote = ref('')
 
