989ae91f4c5d614b9d6e80a048e528529aa16141
flyinacres <ron-github@rfgf.com>
2023-01-11 19:09:03 -0800

    Adding functionality to the notes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -5,24 +5,30 @@
             <div class="field">
                 <div class="control">
                     <textarea 
+                        v-model="newNote"
                         class="textarea" 
+                        ref="newNoteRef"
                         placeholder="Add a new note"/>
                 </div>
             </div>
 
             <div class="field is-grouped is-grouped-right">
                 <div class="control">
-                    <button class="button is-link has-background-success">Add  New Note</button>
+                    <button 
+                        @click="addNote"
+                        :disabled="!newNote"
+                        class="button is-link has-background-success">Add  New Note</button>
                 </div>
             </div>
         </div>
 
         <div 
-            v-for="i in 3" 
+            v-for="note in notes" 
+            :key ="notes.id"
             class="card mb-4">
             <div class="card-content">
                 <div class="content">
-                Veniam mollit exercitation incididunt commodo consectetur officia enim exercitation nostrud cillum elit veniam et. Magna qui esse aute magna. Aliquip ullamco mollit tempor deserunt magna consectetur dolor do ut veniam cillum in qui consequat. Qui elit adipisicing exercitation eu tempor et ad culpa nostrud est. Sint cupidatat laboris velit do sit et minim incididunt laboris veniam. Eu sunt ullamco incididunt deserunt est dolore voluptate non ut voluptate.
+                    {{ note.content }}
                 </div>
             </div>
             <footer class="card-footer">
@@ -32,3 +38,34 @@
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const newNote = ref('')
+
+// Even though I added the ref on the text component, I still have to add this ref or it
+// cannot be accessed
+const newNoteRef = ref(null)
+const notes = ref([
+
+]
+)
+
+const addNote = () => {
+
+    let currentDate = new Date().getTime()
+    let id = currentDate.toString()
+
+    let note = {
+        id: id,
+        content: newNote.value
+    }
+
+    notes.value.unshift(note)
+    newNote.value = ''
+    newNoteRef.value.focus()
+}
+
+
+</script>
