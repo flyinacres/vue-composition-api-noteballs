bd32e34f902e624d2dd8e1734ff3379eff9e80aa
flyinacres <ron-github@rfgf.com>
2023-01-12 13:33:24 -0800

    Fleshing out the new component

---
+++ src/components/Notes/AddEditNote.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card has-background-success-dark p-4 mb-5">
+        <div class="field">
+            <div class="control">
+                <textarea 
+                    :value="props.modelValue"
+                    @input="$emit('update:modelValue', $event.target.value)"
+                    class="textarea" 
+                    ref="textAreaRef"
+                    placeholder="Add a new note"/>
+            </div>
+        </div>
+
+        <div class="field is-grouped is-grouped-right">
+            <div class="control">
+                <slot name="buttons"/>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        required: true
+    }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const textAreaRef = ref(null)
+
+const focusTextArea = () => {
+    textAreaRef.value.focus()
+}
+
+defineExpose({
+    focusTextArea
+})
+</script>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,26 +1,18 @@
 <template>
     <div class="notes">
 
-        <div class="card has-background-success-dark p-4 mb-5">
-            <div class="field">
-                <div class="control">
-                    <textarea 
-                        v-model="newNote"
-                        class="textarea" 
-                        ref="newNoteRef"
-                        placeholder="Add a new note"/>
-                </div>
-            </div>
-
-            <div class="field is-grouped is-grouped-right">
-                <div class="control">
-                    <button 
-                        @click="addNote"
-                        :disabled="!newNote"
-                        class="button is-link has-background-success">Add  New Note</button>
-                </div>
-            </div>
-        </div>
+        <AddEditNote 
+            ref="addEditNoteRef"
+            v-model="newNote">
+            <template #buttons>
+                <button 
+                    @click="addNote"
+                    :disabled="!newNote"
+                    class="button is-link has-background-success">
+                    Add New Note
+                </button>            
+            </template>
+        </AddEditNote>
 
         <Note 
             v-for="note in storeNotes.notes" 
@@ -33,21 +25,19 @@
 <script setup>
 import { ref } from 'vue'
 import Note from '@/components/Notes/Note.vue'
+import AddEditNote from '@/components/Notes/AddEditNote.vue'
 import { useStoreNotes } from '@/stores/storeNotes'
 
-const newNote = ref('')
 
-// Even though I added the ref on the text component, I still have to add this ref or it
-// cannot be accessed
-const newNoteRef = ref(null)
+const newNote = ref('')
+const addEditNoteRef = ref(null)
 
 const storeNotes = useStoreNotes()
 
 const addNote = () => {
     storeNotes.addNote(newNote.value)
-    
     newNote.value = ''
-    newNoteRef.value.focus()
+    addEditNoteRef.value.focusTextArea()
 }
 
 </script>
