18c636df93d57788d3b1e35875038659cf918241
flyinacres <ron-github@rfgf.com>
2023-01-12 17:09:50 -0800

    Save working

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -19,38 +19,42 @@ state: () => {
 },
 actions: {
     addNote(newNoteContent) {
-    let currentDate = new Date().getTime(),
-        id = currentDate.toString()
+        let currentDate = new Date().getTime(),
+            id = currentDate.toString()
 
-    let note = {
-        id,
-        content: newNoteContent
-    }
+        let note = {
+            id,
+            content: newNoteContent
+        }
 
-    this.notes.unshift(note)
-    },
+        this.notes.unshift(note)
+        },
+        
     deleteNote(idToDelete) {
-    this.notes = this.notes.filter(note => note.id !== idToDelete )
-    },
+        this.notes = this.notes.filter(note => note.id !== idToDelete )
+        },
+
     updateNote(id, content) {
-    let index = this.notes.findIndex(note => note.id === id )
-    this.notes[index].content = content
-    }
+        let index = this.notes.findIndex(note => note.id === id )
+        this.notes[index].content = content
+        }
 },
 getters: {
     getNoteContent: (state) => {
-    return (id) => {
-        return state.notes.filter(note => note.id === id )[0].content
-    }
+        return (id) => {
+            return state.notes.filter(note => note.id === id )[0].content
+        }
     },
+
     totalNotesCount: (state) => {
-    return state.notes.length
+        return state.notes.length
     },
+
     totalCharactersCount: (state) => {
-    let count = 0
-    state.notes.forEach(note => {
-        count += note.content.length
-    })
+        let count = 0
+        state.notes.forEach(note => {
+            count += note.content.length
+        })
     return count
     }
 }
--- src/views/ViewEditNote.vue
+++ src/views/ViewEditNote.vue
@@ -9,10 +9,11 @@
             <template #buttons>
                 <button 
                     @click="$router.back()"
-                    class="button is-link is-light">
+                    class="button is-link is-light mr-2">
                     Cancel
                 </button> 
                 <button 
+                    @click="handleSaveClicked"
                     :disabled="!noteContent"
                     class="button is-link has-background-link">
                     Save Note
@@ -25,16 +26,19 @@
 <script setup>
 import AddEditNote from '@/components/Notes/AddEditNote.vue'
 import {ref} from 'vue'
+import {useRoute, useRouter} from  'vue-router'
 import {useStoreNotes} from '@/stores/storeNotes'
 import {storeToRefs} from 'pinia'
 
 const storeNotes = useStoreNotes()
 const noteContent = ref('')
+const route = useRoute()
+const router = useRouter()
 
-const {getNoteContent} = storeToRefs(storeNotes)
-noteContent.value = getNoteContent.value
+noteContent.value = storeNotes.getNoteContent(route.params.id)
 
-
-
-
+const handleSaveClicked = () => {
+    storeNotes.updateNote(route.params.id, noteContent.value)
+    router.push('/')
+}
 </script>
